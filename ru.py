4480bd462daf94ecd803e7049677d2fe5202bdbf
null-0bject <126412888+null-0bject@users.noreply.github.com>
2023-03-25 21:36:08 +0300

    Update ru.py
    comments added

--- ru.py
+++ ru.py
@@ -1,3 +1,20 @@
+'''
+    Вы все делаете на свой страх и риск, при ошибках пишите либо мне - null.object, либо преподавателю, либо решайте сами.
+    
+    1. пройти по C:\Users\имяпользователя\AppData\Local\Programs\Python\Python(версияпайтона)\Lib\site-packages\rinoh\language
+    2. Создать файл ru.py(либо скачать этот файл и вставить его в папку)
+    3. скопировать код описанный ниже
+    4. в этой же папке найти __init__.py
+    5. в импорты вписать строчку
+        from .ru import RU
+    таким образом вы импротируете язык
+    6. в свой config.py в то место, где у вас конфиги rinoh вписывайте
+        rinoh.language.RU
+    7. Проверяйте созданием PDF документа.
+
+
+
+'''
 from .cls import Language
 from ..structure import SectionTitles, AdmonitionTitles
 
